6b21a1c231c3ef70faa6970a47a4615a947c319a
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-20 17:58:21 +0300

    learned about php data types
    	learned about php arrays
    			learned about php classes and objects

---
+++ dataTypes.php
@@ -0,0 +1,64 @@
+<!--String
+Integer
+Float (floating point numbers - also called double)
+Boolean
+Array
+Object
+NULL
+Resource-->
+
+<!--PHP String-->
+<?php
+$x = "Hello world!";
+$y = 'Hello world!';
+
+echo $x;
+echo "<br>";
+echo $y;
+?>
+
+<!--PHP Integer-->
+<?php
+$x = 5985;
+var_dump($x);
+?>
+
+<!--PHP Float-->
+<?php
+$x = 10.365;
+var_dump($x);
+?>
+
+<!--php arrays-->
+<?php
+$cars = array("Volvo","BMW","Toyota");
+var_dump($cars);
+?>
+
+<!--PHP Object-->
+<?php
+class Car {
+    public $color;
+    public $model;
+    public function __construct($color, $model) {
+        $this->color = $color;
+        $this->model = $model;
+    }
+    public function message() {
+        return "My car is a " . $this->color . " " . $this->model . "!";
+    }
+}
+
+$myCar = new Car("black", "Volvo");
+echo $myCar -> message();
+echo "<br>";
+$myCar = new Car("red", "Toyota");
+echo $myCar -> message();
+?>
+<!--null values-->
+<?php
+$x = "Hello world!";
+$x = null;
+var_dump($x);
+?>
+
