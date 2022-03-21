45fbe3b2139d7b9ff32e5b4029b79a7afd1e2cdf
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-21 10:22:04 +0300

    learned about different functions in php to do mathematical operations

---
+++ math.php
@@ -0,0 +1,13 @@
+<?php
+//PHP abs() Function
+echo abs(-122.3);
+//PHP sqrt() Function
+echo"</br>". sqrt(2);
+//PHP round() Function
+echo"<br>". round(12.2);
+echo"</br>".round(12.8);
+
+
+//Random Numbers
+echo"</br>".rand(1,2);
+?>
