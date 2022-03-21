691cf1db3ebf6441e22c96cc179e54a013c8f341
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-21 11:05:57 +0300

    learned about switch statement and how to use it

---
+++ switch.php
@@ -0,0 +1,13 @@
+<!--Use the switch statement to select one of many blocks of code to be executed.-->
+<?php
+$favcolor = "red";
+switch ($favcolor){
+    default :echo "ew idk what even is that ";
+    case "red": echo "wow your favourite color is red! i dont like red";
+    break;
+    case "yellow":echo"I like yellow but I wanted you to be different";
+    break;
+    case "blue":echo "bruh blue is like the best color";
+
+}
+?>
