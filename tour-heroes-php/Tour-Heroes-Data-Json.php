e3342128abcc4663e8aae616d81379ffe6f8f088
OmarAlomari <omar.alomari@tickerchart.com>
2022-03-28 14:23:07 +0300

    changed while approach using mysqli fetch result to fetch_all

--- tour-heroes-php/Tour-Heroes-Data-Json.php
+++ tour-heroes-php/Tour-Heroes-Data-Json.php
@@ -1,14 +1,14 @@
 <?php
+header("Access-Control-Allow-Origin: *");
 include_once 'connectToDB.php';//connection to tour-heroes database
 
 $sql = "select * from Heroes;";
 $results = mysqli_query($connectionToDatabase, $sql);
 
-$heroesArray = array();
-while ($row = mysqli_fetch_assoc($results)){
-    $heroesArray[] = $row;
-}
+//$heroesArray =$results ->fetch_all(MYSQLI_ASSOC);
+$heroesArray = mysqli_fetch_all($results, MYSQLI_ASSOC);
+
 $connectionToDatabase->close();
 
 header('Content-type: application/json');
-echo (json_encode($heroesArray));
+echo json_encode($heroesArray);
