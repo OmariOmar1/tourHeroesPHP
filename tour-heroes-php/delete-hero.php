e9a68768d1231f0711138d5b9a203cd48bf16a59
OmarAlomari <omar.alomari@tickerchart.com>
2022-04-04 15:59:34 +0300

    stopped getting data from global variable and started taking the raw data of the request
    renamed file from all-heroes ->list-heroes

--- tour-heroes-php/delete-hero.php
+++ tour-heroes-php/delete-hero.php
@@ -1,6 +1,6 @@
 <?php
 include_once 'db-connect.php';
-$selectedHero=$_POST['HeroId'];
+$selectedHero = file_get_contents('php://input');
 $sql = "Delete from Heroes WHERE HeroId =$selectedHero;";
 if(mysqli_query($connectionToDatabase, $sql)){
     echo "true";
