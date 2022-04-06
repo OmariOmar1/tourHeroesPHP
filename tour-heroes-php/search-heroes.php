0e9d1611d4c826f35eab350351cc93248e5d6886
OmarAlomari <omar.alomari@tickerchart.com>
2022-04-06 14:40:09 +0300

    added search file

---
+++ tour-heroes-php/search-heroes.php
@@ -0,0 +1,16 @@
+<?php
+//headers
+header('Access-Control-Allow-Origin:*');
+
+//connect to database
+include_once 'db-connect.php';
+include 'cors.php';
+if (isset($_GET)) {
+    $searchTerm = $_GET['HeroFirstName'];
+    $sql = "select * from Heroes WHERE HeroFirstName LIKE '$searchTerm%';";
+    $results = mysqli_query($connectionToDatabase, $sql);
+    $hero = mysqli_fetch_all($results,MYSQLI_ASSOC);
+
+    header('Content-Type: application/json; charset=utf-8');
+    echo json_encode($hero);
+}
